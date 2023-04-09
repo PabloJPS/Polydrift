fa34a91dcc5c084a3a4ff095c7df32d31edf0dd0
jesse0099 <jesechavez009@gmail.com>
2023-04-09 16:55:46 -0600

    car movement using physics + road repair

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -1,40 +1,32 @@
-using System.Collections;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float maxSpeed = 30f; // The maximum speed of the car
-    public float acceleration = 10f; // The acceleration of the car
+    public float maxSpeed = 5000f; // The maximum speed of the car
+    public float acceleration = 300f; // The acceleration of the car
     public float turningSpeed = 100f; // The speed at which the car turns
+    public float brakeStrength = 100f; // The speed at which the car turns
+
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.mass = 10; // Set the mass of the Rigidbody component to 1000
-        rb.drag = 0.5f; // Set the drag value of the Rigidbody component
-        rb.angularDrag = 0.5f; // Set the angular drag value of the Rigidbody component
+        rb.mass = 9000; // Set the mass of the Rigidbody component to 1000
+        rb.drag = 0f; // Set the drag value of the Rigidbody component
+        rb.angularDrag = 0f; // Set the angular drag value of the Rigidbody component
     }
 
     private void Update()
     {
-        // float horizontalInput = Input.GetAxis("Horizontal");
-        // float verticalInput = Input.GetAxis("Vertical");
 
-        // // Get the ride's forward direction in local space
-        // Vector3 rideForward = transform.TransformDirection(Vector3.forward);
+    }
 
-        // // Move the ride forward or backward based on vertical input
-        // Vector3 movement = rideForward * -verticalInput * 200 * Time.deltaTime;
-        // rb.MovePosition(transform.position + movement);
-
-        // // Turn the ride based on horizontal input
-        // float turnAmount = horizontalInput * 20 * Time.deltaTime;
-        // Quaternion turnRotation = Quaternion.Euler(0f, turnAmount, 0f);
-        // rb.MoveRotation(rb.rotation * turnRotation);
-        
+    private void FixedUpdate()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        bool brakeInput = Input.GetButton("Jump");
 
         // Get the ride's forward direction in local space
         Vector3 rideForward = transform.TransformDirection(Vector3.forward);
@@ -47,14 +39,23 @@ public class CarController : MonoBehaviour
         float maxSpeedPerFrame = maxSpeed * Time.deltaTime;
         float newSpeed = Mathf.Clamp(currentSpeed + accelerationAmount, -maxSpeedPerFrame, maxSpeedPerFrame);
 
-        // Move the ride forward or backward based on the new speed
-        Vector3 movement = rideForward * newSpeed;
-        rb.MovePosition(transform.position + movement);
+        // Calculate the force needed to achieve the desired speed
+        float forceMagnitude = (newSpeed - currentSpeed) / Time.deltaTime * rb.mass;
+
+        // Apply the force to the ride's Rigidbody component
+        Vector3 movementForce = rideForward * forceMagnitude;
+        rb.AddForce(movementForce);
+
+        // Apply a brake force if the brake input is detected
+        if (brakeInput)
+        {
+            Vector3 brakeForce = -rb.velocity.normalized * brakeStrength * rb.mass;
+            rb.AddForce(brakeForce);
+        }
 
         // Turn the ride based on horizontal input
         float turnAmount = horizontalInput * turningSpeed * Time.deltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turnAmount, 0f);
         rb.MoveRotation(rb.rotation * turnRotation);
     }
-
 }
