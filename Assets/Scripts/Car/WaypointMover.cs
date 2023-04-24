ec50491739fef646b06ebab62eb94858205ef1ff
dhernandez0599@gmail.com <47802332+LinuxPingu@users.noreply.github.com>
2023-04-24 11:50:11 -0600

    Enemy Car Paths, Main Car Values changed

---
+++ Assets/Scripts/Car/WaypointMover.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMover : MonoBehaviour
+{
+    [SerializeField] private Waypoint waypoints;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float distance = 0.1f;
+    private Transform current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = waypoints.GetNextWaypoint(current);
+        transform.position = current.position;
+
+        current = waypoints.GetNextWaypoint(current);
+        transform.LookAt(current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, current.position,moveSpeed);
+        if (Vector3.Distance(transform.position,current.position) < distance) 
+        {
+            current = waypoints.GetNextWaypoint(current);
+            transform.LookAt(current);
+        }
+    }
+}
